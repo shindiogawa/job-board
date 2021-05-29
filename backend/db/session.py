240c2d0634adabb31fd12de77e8b3d1ae5b2fc1a
rodrigogawa <56047207+rodrigogawa@users.noreply.github.com>
2021-05-28 23:25:26 -0300

    Created schema for users validation

--- backend/db/session.py
+++ backend/db/session.py
@@ -1,6 +1,6 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-
+from typing import Generator
 from core.config import settings
 
 SQLALCHEMY_DATABASE_URL = settings.DATABASE_URL
@@ -12,3 +12,10 @@ engine = create_engine(SQLALCHEMY_DATABASE_URL)
 #   SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
 # )
 SessionLocal = sessionmaker(autocommit = False, autoflush = False, bind = engine)
+
+def get_db() -> Generator: 
+  try:
+    db = SessionLocal()
+    yield db
+  finally:
+    db.close()
