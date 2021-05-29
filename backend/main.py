b57d39ef5bace9c1a0c24ba33c5ed1994e7e718f
rodrigogawa <56047207+rodrigogawa@users.noreply.github.com>
2021-05-28 22:03:58 -0300

    Setup settings file

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
+from .core.config import settings
 
-
-app = FastAPI(title="Jobboard", version="0.1.0")
+app = FastAPI(title=settings.PROJECT_TITLE, version=settings.PROJECT_VERSION)
 
 
 @app.get("/")
