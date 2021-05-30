4f90109ef33cc2b45c2240e698b8064bef68ff9e
rodrigogawa <56047207+rodrigogawa@users.noreply.github.com>
2021-05-29 23:26:53 -0300

    Created logic to generate the authentication token

---
+++ backend/apis/version1/route_login.py
@@ -0,0 +1,35 @@
+from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordRequestForm
+from fastapi import Depends
+from fastapi import APIRouter
+from sqlalchemy.orm import Session
+from datetime import timedelta
+from backend.db.session import get_db
+from backend.core.config import settings
+from backend.core.security import create_access_token
+from backend.db.repository.login import get_user
+from backend.core.hashing import Hasher
+
+router = APIRouter(
+  prefix="/login",
+  tags=["login"]
+)
+
+def authenticate_user(username: str, password: str, db: Session):
+  user = get_user(username=username, db=db)
+  print(user)
+  if not user:
+    return False
+  if not Hasher.verify_password(password, user.hashed_password):
+    return False
+  return user
+
+@router.post("/token")
+def login_for_access_token(form_data:OAuth2PasswordRequestForm=Depends(), db: Session = Depends(get_db)):
+  user = authenticate_user(form_data.username, form_data.password, db)
+  if not user:
+    raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, 
+    detail="Incorrect username or password")
+  access_token_expire = timedelta(minutes = settings.ACCESS_TOKEN_EXPIRES_MINUTE)
+  access_token = create_access_token(data = {"sub": user.email}, expires_delta = access_token_expire)
+  return {"access_token": access_token, "token_type": "bearer"}
