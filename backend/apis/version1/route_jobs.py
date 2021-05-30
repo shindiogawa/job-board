9b037bbb7e3071b3b2bb928b898acae7dd85fb2a
rodrigogawa <56047207+rodrigogawa@users.noreply.github.com>
2021-05-29 21:02:33 -0300

    Created endpoints to retrieve a specific job by id and all jobs where is_active

--- backend/apis/version1/route_jobs.py
+++ backend/apis/version1/route_jobs.py
@@ -1,10 +1,11 @@
+from typing import List
 from fastapi import APIRouter, Depends, HTTPException, status
-from sqlalchemy.orm import Session
+from sqlalchemy.orm import Session, session
 
 from backend.db.session import get_db
 from backend.db.models.jobs import Job
 from backend.schemas.jobs import JobCreate, ShowJob
-from backend.db.repository.jobs import create_new_job
+from backend.db.repository.jobs import create_new_job, retrieve_job, list_jobs
 
 router = APIRouter(
     prefix="/job",
@@ -16,3 +17,17 @@ def create_job(job: JobCreate, db: Session = Depends(get_db)):
   owner_id = 1
   job = create_new_job(job=job, db=db, owner_id=owner_id)
   return job
+
+@router.get("/get/{id}", response_model=ShowJob)
+def retrieve_job_by_id(id:int, db: Session = Depends(get_db)):
+  job = retrieve_job(id=id, db=db)
+  if not job:
+    raise HTTPException(  status_code=status.HTTP_404_NOT_FOUND,
+                    detail=f"Job with id {id} does not exist"
+                  )
+  return job
+
+@router.get("/all", response_model=List[ShowJob])
+def retrieve_all_jobs(db:Session = Depends(get_db)):
+  jobs = list_jobs(db=db)
+  return jobs
