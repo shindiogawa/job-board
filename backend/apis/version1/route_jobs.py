bd73de98c66e8d58dae3efcb2a37fdda3f3b6714
rodrigogawa <56047207+rodrigogawa@users.noreply.github.com>
2021-05-29 21:11:16 -0300

    Created endpoint to update a job

--- backend/apis/version1/route_jobs.py
+++ backend/apis/version1/route_jobs.py
@@ -5,7 +5,7 @@ from sqlalchemy.orm import Session, session
 from backend.db.session import get_db
 from backend.db.models.jobs import Job
 from backend.schemas.jobs import JobCreate, ShowJob
-from backend.db.repository.jobs import create_new_job, retrieve_job, list_jobs
+from backend.db.repository.jobs import create_new_job, retrieve_job, list_jobs, update_job_by_id
 
 router = APIRouter(
     prefix="/job",
@@ -31,3 +31,12 @@ def retrieve_job_by_id(id:int, db: Session = Depends(get_db)):
 def retrieve_all_jobs(db:Session = Depends(get_db)):
   jobs = list_jobs(db=db)
   return jobs
+
+@router.put("/update/{id}")
+def update_job(id: int, job:JobCreate, db: Session=Depends(get_db)):
+  owner_id = 1
+  message = update_job_by_id(id=id, job=job, db=db, owner_id=owner_id)
+  if not message:
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, 
+    detail="Job with id {id} does not exist")
+  return {"detail": "Successfully updated data."}
