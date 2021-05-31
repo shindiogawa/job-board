adb74ec09e7e07ad9eea54fee965680e7dc6131d
rodrigogawa <56047207+rodrigogawa@users.noreply.github.com>
2021-05-31 18:23:31 -0300

    Added detailview of jobs

--- backend/webapps/jobs/route_jobs.py
+++ backend/webapps/jobs/route_jobs.py
@@ -5,6 +5,8 @@ from fastapi.templating import Jinja2Templates
 from backend.db.repository.jobs import list_jobs
 from sqlalchemy.orm import Session
 from backend.db.session import get_db
+from backend.db.repository.jobs import retrieve_job
+
 templates = Jinja2Templates(directory="backend/templates")
 
 router = APIRouter(
@@ -18,3 +20,8 @@ def home(request: Request, db: Session = Depends(get_db)):
   jobs = list_jobs(db=db)
   
   return templates.TemplateResponse("jobs/homepage.html", {"request": request, "jobs": jobs})
+
+@router.get("/detail/{id}")
+def jobs_detail(id:int, request: Request, db: Session = Depends(get_db)):
+  job = retrieve_job(id=id, db=db)
+  return templates.TemplateResponse("jobs/detail.html",{"request": request, "job": job})
