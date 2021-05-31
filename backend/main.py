72e5b62852a70dedac6b2c7f91f37e36721ce151
rodrigogawa <56047207+rodrigogawa@users.noreply.github.com>
2021-05-31 17:56:52 -0300

    Serving static files with fastapi

--- backend/main.py
+++ backend/main.py
@@ -4,7 +4,7 @@ from backend.db.session import engine
 from backend.db.base import Base
 from backend.apis.base import api_router
 from backend.webapps.base import api_router as webapp_router
-
+from fastapi.staticfiles import StaticFiles
 
 
 def create_tables():
@@ -14,11 +14,14 @@ def include_router(app):
   app.include_router(api_router)
   app.include_router(webapp_router)
   
+def configure_static(app):
+  app.mount("/static", StaticFiles(directory="backend/static"), name="static")
 
 def start_application():
   app = FastAPI(title=settings.PROJECT_TITLE, version=settings.PROJECT_VERSION)
   create_tables()
   include_router(app)
+  configure_static(app)
   return app
 
 app = start_application()
