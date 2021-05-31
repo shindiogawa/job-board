72dbc1ca1fb6ffbf299e7cd7bf32f2647eee0187
rodrigogawa <56047207+rodrigogawa@users.noreply.github.com>
2021-05-31 18:42:53 -0300

    Created form for registration and router

--- backend/webapps/base.py
+++ backend/webapps/base.py
@@ -1,8 +1,9 @@
 from fastapi import APIRouter
 
 from backend.webapps.jobs import route_jobs
-
+from backend.webapps.users import route_users
 api_router = APIRouter()
 
 api_router.include_router(route_jobs.router)
+api_router.include_router(route_users.router)
 
---
+++ backend/webapps/users/route_users.py
@@ -0,0 +1,14 @@
+from fastapi import APIRouter
+from fastapi.templating import Jinja2Templates
+from fastapi import Request
+
+router = APIRouter(
+  include_in_schema=False,
+  tags=["users"]
+)
+
+templates = Jinja2Templates(directory="backend/templates")
+
+@router.get("/register/")
+def register(request: Request):
+  return templates.TemplateResponse("users/register.html", {"request": request})
